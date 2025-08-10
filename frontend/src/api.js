6d18c7e853929952c5cf62c05f98d9f540d4f1f4
Lingaraj Patil <153691551+Lingaraj-Patil@users.noreply.github.com>
2025-08-10 16:46:22 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://lost-link-323.vercel.app/' });
+const API = axios.create({ baseURL: 'https://lost-link-323.vercel.app/api' });
 
 // Lost Items
 export const fetchLostItems = () => API.get('/lost');
